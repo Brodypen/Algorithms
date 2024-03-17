9973ef3cb79c9206648f8769df477caa5b2400da
Brodypen <brodypen7987@gmail.com>
2024-03-16 21:49:52 -0500

    Finish q1

---
+++ LeetCode/weekly-contest/weekly-contest-389/100248.py
@@ -0,0 +1,29 @@
+# 100248. Existence of a Substring in a String and Its Reverse
+
+class Solution:
+    def isSubstringPresent(self, s: str) -> bool:
+        # Theory
+        # We can use two pointers, in which we check if left matches right substring
+#         l, r = 0, len(s) - 1
+#         while l < r:
+#             print(s[l] + s[l+1])
+#             print(s[r-1] + s[r])
+#             if(s[l] + s[l+1] == s[r-1] + s[r]):
+#                 return True
+#             l+=1
+#             r-=1
+        
+#         return False
+        # Other method, just put every substring of length 2 in set. check if it exist in set
+        subStringSet = set()
+        for i in range(len(s)-1):
+            subStringSet.add(s[i] + s[i+1])
+        # print(subStringSet)
+        for i in range(len(s)-1, 0, -1):
+            # print(s[i] + s[i-1])
+            if(str(s[i] + s[i-1]) in subStringSet):
+                return True
+        return False
+
+input = "qg"
+print(Solution().isSubstringPresent(input))
