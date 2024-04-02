5fcd7be499797e05016d5d2758a93dd963e60b58
Brody <brodypen7987@gmail.com>
2024-04-02 16:30:25 -0500

    4.01

--- EPI_Python/EPIJudge/problem_mapping.js
+++ EPI_Python/EPIJudge/problem_mapping.js
@@ -24,7 +24,7 @@ problem_mapping = {
                 "total": 10000
             },
             "Python: parity.py": {
-                "passed": 0,
+                "passed": 10000,
                 "total": 10000
             }
         },
