a5190e83fc58d09c60b7957405d3fc5310270407
Brody <brodypen7987@gmail.com>
2024-04-05 19:03:40 -0500

    Skipped reverse bits

--- EPI_Python/EPIJudge/epi_judge_python/reverse_bits.py
+++ EPI_Python/EPIJudge/epi_judge_python/reverse_bits.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # Okay, interesting question.
+    # This question requres PRECOMPUTED_REVERSe which is every value for 2^16 reversed.
+    # It's a lookup table. It's massive, I rather not copy it.
+
+    MASK_SIZE = 16
+    BIT_MASK = 0xFFFF
+    return (PRECOMPUTED_REVERSE[x & BIT_MASK] << (3 * MASK_SIZE) |
+            PRECOMPUTED_REVERSE[(x >> MASK_SIZE) & BIT_MASK] << (2 * MASK_SIZE) |
+            PRECOMPUTED_REVERSE[(x >> (2 * MASK_SIZE)) & BIT_MASK] << MASK_SIZE |
+            PRECOMPUTED_REVERSE[(x >> (3 * MASK_SIZE)) & BIT_MASK])
 
 
 if __name__ == '__main__':
